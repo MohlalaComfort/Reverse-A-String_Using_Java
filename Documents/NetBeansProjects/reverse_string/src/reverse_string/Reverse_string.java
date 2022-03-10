16b5fa1071d825be1a065744ebbfccfbd900883e
Mohlala_COMFORT <Mohlala_COMFORT@LAPTOP-JG7AHN42>
2022-03-10 10:58:01 +0200

    initial commit

--- Documents/NetBeansProjects/reverse_string/src/reverse_string/Reverse_string.java
+++ Documents/NetBeansProjects/reverse_string/src/reverse_string/Reverse_string.java
@@ -3,6 +3,7 @@ package reverse_string;
 public class Reverse_string {
 
     public static void main(String[] args) {
+        //declare and assign
         String name = "Comfort";
         
         for(int i=name.length()-1;i>=0;i--){
